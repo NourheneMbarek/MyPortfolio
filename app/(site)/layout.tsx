8788287ea24338318e22a730aee50682861046f8
NourheneMbarek <nourhene96mbarek@gmail.com>
2024-03-03 11:04:21 +0100

    change title

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -7,12 +7,12 @@ import Footer from "./components/global/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sanity Next.js Portfolio Site",
+  title: "Portfolio Nourhene",
   metadataBase: new URL("https://sanity-nextjs-site.vercel.app"),
   description: "A personal portfolio site built with Sanity and Next.js",
   openGraph: {
     images:
-      "https://res.cloudinary.com/victoreke/image/upload/v1689893059/docs/og.png",
+      "",
   },
 };
 
